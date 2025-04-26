07a9795e9b8f83c52c3afd68eddcf31c5ac9181e
ejhib <ejh6207@psu.edu>
2025-04-26 12:10:13 -0400

    main.py now installs streamlit and pandas if it doesn't find it installed on your device automatically.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
 import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+try:
+    import streamlit
+except ImportError:
+    print("Streamlit not found. Installing...")
+    install("streamlit")
+    import streamlit
+
+try:
+    import pandas
+except ImportError:
+    print("Pandas not found. Installing...")
+    install("pandas")
+    import pandas
 
 subprocess.run(["py", "-m", "streamlit", "run", "streamlit_app.py"])
