e6c063358f4d939ff0b8312f8aa22453b6a6f641
sqwidwierd <sqwidwierd@gmail.com>
2025-04-27 18:41:10 -0400

    Added a build_edges function to the MovieGraph class.

--- src/graph/MovieGraph.py
+++ src/graph/MovieGraph.py
@@ -24,3 +24,23 @@ class MovieGraph:
             movie_title = self.movies[movie_id].title
             connections = [(self.movies[n_id].title, w) for n_id, w in self.edges[movie_id]]
             print(movie_title + " --> " + str(connections))
+
+    def build_edges(self):
+        movie_ids = list(self.movies.keys())
+        for i in range(len(movie_ids) - 1):
+            for j in range(i + 1, len(movie_ids)):
+                id1 = movie_ids[i]
+                id2 = movie_ids[j]
+                tags1 = set(self.movies[id1].tags)
+                tags2 = set(self.movies[id2].tags)
+
+                shared_tags = 0
+
+                for tag in tags1:
+                    if tag in tags2:
+                        shared_tags += 1
+                        if shared_tags == 3:
+                            break
+
+                if shared_tags > 0:
+                    self.add_edge(id1, id2, shared_tags)
